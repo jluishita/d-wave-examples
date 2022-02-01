83b1f9c7589eff7b038b349f85594cee3e9d2bf0
Jorge Luis Hita, Ph.D <jluishita@gmail.com>
2022-02-01 17:37:28 +0100

    added reshaped solution

--- knapsack/knapsack-CQM.py
+++ knapsack/knapsack-CQM.py
@@ -39,7 +39,7 @@ def test_unique_color(data, n_regions):
     if np.array_equal(np.ones(n_regions), data.sum(axis=1)):
         print(' \nConstraint #1 is fulfilled.\n')
     else:
-        print(' \nWarning: Constraint #1 is NOT fulfilled\.n')
+        print(' \nWarning: Constraint #1 is NOT fulfilled.\n')
 
 def test_adjacent_regions(list_of_regions, color_to_region, list_of_borders):
     """
@@ -67,9 +67,10 @@ def main():
     # Defines the nweights for each object
     list_of_weights = [2, 4, 1.8, 1.6]
 
+    # Defines the upper value of the weights of the subset
     max_weight = 7.8
 
-    # Number of colors and number of regions
+    # Number of objects
     n_objects = len(list_of_objects)
 
     # Defines the array of binary variables
@@ -78,8 +79,10 @@ def main():
     # Declares the cqm object
     cqm = ConstrainedQuadraticModel()
 
+    # Defines the objective function
     cqm.set_objective(quicksum(-x[object]*list_of_values[object] for object in range(n_objects)))
 
+    # Defines the inequality constraint: the sum of the weights must not exceed max_weight
     cqm.add_constraint(quicksum(x[object]*list_of_weights[object] for object in range(n_objects)) <= max_weight, label='max_weight')
 
     # Declare the sample to be used and solve the problem
@@ -90,8 +93,12 @@ def main():
     feasible_sols = np.where(sampleset.record.is_feasible==True)
     solution = sampleset.record[feasible_sols[0][0]][0]
 
+    # Reshapes the final solution
+    df_sol = pd.DataFrame({'# of elements':list_of_elements, 'value':list_of_values, 
+                           'weight':list_of_weights, '# of elements in solution':solution}, index=list_of_objects)
+
     # Prints final solution
-    print(solution)
+    print(df_sol)
     
 	
 if __name__=="__main__":
