0cb633fc67e3389d8e304e82f30a7d16f64571ab
Jorge Luis Hita, Ph.D <jluishita@gmail.com>
2022-02-01 09:36:42 +0100

    improved for loops and improved comments

--- map-coloring/map-coloring-BQM.py
+++ map-coloring/map-coloring-BQM.py
@@ -11,7 +11,8 @@
  Date: 26th January 2022
 
  Description: This program solves naïve instances of the Map Coloring 
-     problem using D-Wave's Quantum Annealer.
+     problem on the map of the regions of France using D-Wave's 
+     Quantum Annealer. The map consists of 12 regions.
 
      A Map Coloring problem is a problem of assigning colors to a set of 
      regions, subject to the restriction that adjacent regions can not be
@@ -22,7 +23,7 @@
 
      In this file, the Binary Quadratic Model sampler (BQM) is used.
 
-Disclaimer: This code is an experimental, not-optimized, not-tested code.
+ Disclaimer: This code is an experimental, not-optimized, not-tested code.
      USE IT AT YOUR OWN RISK.
 
 """
@@ -35,15 +36,20 @@ from dwave.system import DWaveSampler, EmbeddingComposite
 from dimod import BinaryQuadraticModel
 
 def test_unique_color(data, n_regions):
+    """
+    This function checks whether the first constraint is fulfilled
+    """
     if np.array_equal(np.ones(n_regions), data.sum(axis=1)):
         print(' \nConstraint #1 is fulfilled.\n')
     else:
         print(' \nWarning: Constraint #1 is NOT fulfilled\.n')
 
 def test_adjacent_regions(list_of_regions, color_to_region, list_of_borders):
-    for region1 in list_of_regions:
+    """
+    This function checks whether the second constriant is fulfilled
+    """
+    for r1, region1 in enumerate(list_of_regions):
         for region2 in list_of_borders[region1]:
-            r1 = list_of_regions.index(region1)
             r2 = list_of_regions.index(region2)
             if color_to_region[r1]==color_to_region[r2]:
                 print(' \nWarning: Constraint #2 is NOT fulfilled.\n')
@@ -76,6 +82,7 @@ def main():
                                          "Grand-Est", "Hauts-de-France"],
                        "Grand-Est": ["Hauts-de-France", "Île-de-France", "Bourgogne Franche-Comté"],
                        "Hauts-de-France": ["Normandie", "Île-de-France", "Grand-Est"]}
+
     # Defines the lagrange multipliers for the constraints
     lag_mul_1 = 1
     lag_mul_2 = 1
@@ -86,28 +93,27 @@ def main():
     # Declares the bqm object
     bqm = BinaryQuadraticModel('BINARY')
 
-    # Define the constraint 1: Any region must be assigned to one and only one color
+    # Defines the constraint #1: Any region must be assigned to one and only one color
     for region in range(n_regions):
         c_weights = [(x[region][color], 1) for color in range(n_colors)]
         bqm.add_linear_equality_constraint(c_weights, constant=-1, lagrange_multiplier=lag_mul_1)
 
-    # Define the constraint 2: Any order must be selected once and only once
+    # Defines the constraint #2: Two adjacent regions cannot be assigned to the same color
     visited = set()
-    for region1 in list_of_regions:
+    for r1, region1 in enumerate(list_of_regions):
         for region2 in list_of_borders[region1]:
             if (region1,region2) not in visited and (region2,region1) not in visited:
                 visited.add((region1,region2))
-                r1 = list_of_regions.index(region1)
                 r2 = list_of_regions.index(region2)
                 for color in range(n_colors):
                     bqm.add_quadratic(x[r1][color], x[r2][color], lag_mul_2)
 
 
-    # Declare the sample to be used and solve the problem
+    # Declares the sample to be used and solve the problem
     sampler = EmbeddingComposite(DWaveSampler())
     sampleset = sampler.sample(bqm, num_reads=100)
 
-    # Take best solution
+    # Takes best solution
     first_sample = sampleset.first.sample
 
     # Reshapes the best solution
@@ -116,11 +122,14 @@ def main():
         for region in range(n_regions):
             data[region, color] = first_sample[x[region][color]]
 
+    # Checks the constriant #1
     test_unique_color(data, n_regions)
 
+    # Defines the list of colors assigned to each region
     list_of_colors_np = np.array(list_of_colors)
     color_to_region = list_of_colors_np[np.argmax(data, axis=1)]
 
+    # Checks the constrint #2
     test_adjacent_regions(list_of_regions, color_to_region, list_of_borders)
 
     # Defines column and row names for the final solution
